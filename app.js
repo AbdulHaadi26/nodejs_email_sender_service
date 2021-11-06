8c28ac89017b620ec312b79f82498dbe8aea2bac
Abdul Haadi <abdulhaadirao@gmail.com>
2021-11-06 14:52:06 +0500

    Added Base64 Support

--- app.js
+++ app.js
@@ -166,7 +166,6 @@ app.post('/apply', async (req, res) => {
     }
 });
 
-
 app.post('/qoute', async (req, res) => {
     try {
         const { first_name, last_name, email, location, phone, service, info } = req.body;
@@ -296,6 +295,201 @@ async function googleSpreadSheet(data) {
     }
 }
 
-http.createServer(app).listen(process.env.PORT);
+
+app.post('/stormer/send/mail', async (req, res) => {
+    try {
+
+        let {
+            datum, uhrzeit, mitarbeiter, minuten, unterschrankband, hāngeschrankband, rating, free_text, signature,
+            seitenschrankband, vanReit, forentenstern, laserGroദ, klein, korpusfertigung, APLBAZ
+        } = req.body;
+
+        let text = '';
+
+        if (unterschrankband && unterschrankband.length > 0) {
+            unterschrankband.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+        unterschrankband = text;
+
+        text = ''
+        if (hāngeschrankband && hāngeschrankband.length > 0) {
+            hāngeschrankband.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+        hāngeschrankband = text;
+
+        text = ''
+        if (laserGroദ && laserGroദ.length > 0) {
+            laserGroദ.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+        laserGroദ = text;
+
+        text = ''
+        if (seitenschrankband && seitenschrankband.length > 0) {
+            seitenschrankband.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+        seitenschrankband = text;
+
+        text = ''
+        if (vanReit && vanReit.length > 0) {
+            vanReit.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+
+        vanReit = text;
+
+
+        text = ''
+        if (forentenstern && forentenstern.length > 0) {
+            forentenstern.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+
+        forentenstern = text;
+
+        text = ''
+        if (klein && klein.length > 0) {
+            klein.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+
+        klein = text;
+
+        text = ''
+        if (korpusfertigung && korpusfertigung.length > 0) {
+            korpusfertigung.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+
+        korpusfertigung = text;
+
+        text = ''
+        if (APLBAZ && APLBAZ.length > 0) {
+            APLBAZ.map(i => {
+                text = text + i.toString();
+                text = text + '<br/>';
+                return i;
+            });
+        }
+
+        APLBAZ = text;
+        var transporter = nodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASS
+            }
+        });
+
+        var mailOptions = {
+            from: process.env.EMAIL,
+            to: process.env.EMAIL,
+            subject: 'Stormer-AG',
+            html: `
+            <div style="display: flex; flex-direction: row; justify-content: center;">
+            <h1 style="textAlign: center; font-weight: 400">Details</h1>
+       </div>
+       <div style="display: flex; flex-direction: row; justify-content: space-between;">
+            <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Datum:</span> ${datum}</h4>
+            <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600; margin-left: 120px;">Uhrzeit:</span> ${uhrzeit}</h4>
+       </div>
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Mitarbeiter:</span> ${mitarbeiter}</h4>
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Benötigte Wartungzeist in Minuten:</span> ${minuten}/180 minuten</h4>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Unterschrankband:</span>
+       <br/>
+       <p style="font-weight: 400">${unterschrankband}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Hāngeschrankband:</span>
+       <br/>
+       <p style="font-weight: 400">${hāngeschrankband}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Seitenschrankband:</span>
+       <br/>
+       <p style="font-weight: 400">${seitenschrankband}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">VanReit:</span>
+       <br/>
+       <p style="font-weight: 400">${vanReit}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Forentenstern:</span>
+       <br/>
+       <p style="font-weight: 400">${forentenstern}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Laser Groദ:</span>
+       <br/>
+       <p style="font-weight: 400">${laserGroദ}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Klein:</span>
+       <br/>
+       <p style="font-weight: 400">${klein}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Korpusfertigung:</span>
+       <br/>
+       <p style="font-weight: 400">${korpusfertigung}</p>
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">APL BAZ:</span>
+       <br/>
+       <p style="font-weight: 400">${APLBAZ}</p>
+       
+       
+       
+       <h4 style="textAlign: center; font-weight: 400"><span style="font-weight: 600">Gesamtzustand Maschinenpark heute:</span> ${rating}/5</h4>
+            `,
+            attachments: [
+                {
+                    filename: 'free-text.png',
+                    content: free_text.split("base64,")[1],
+                    encoding: 'base64'
+                },
+                {
+                    filename: 'signature.png',
+                    content: signature.split("base64,")[1],
+                    encoding: 'base64'
+                },
+            ]
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                throw new Error('Email not sent.')
+            }
+
+            return res.json({ success: true });
+        });
+    } catch (e) {
+        console.log(e);
+        res.json({ error: e.message });
+    }
+});
+http.createServer(app).listen(process.env.PORT || 80);
 
 
